c87ada2c6e70385caf8f815ad14a36c10e2235ce
Matthew McLendon <matthewmclendon1006@gmail.com>
2023-10-13 15:40:41 -0500

    func: updated navbar with dynamic links based on user

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -10,17 +10,38 @@ export default function Navbar() {
     logOutUser();
   };
 
+  const links = user ? (
+    <ul className="links-list">
+      <li>
+        <Link to={"/home"}>Home</Link>
+      </li>
+      <li>
+        <Link to={"/subscriptions"}>Subscriptions</Link>
+      </li>
+      <li>
+        <Link to={"/profile"}>Profile</Link>
+      </li>
+    </ul>
+  ) : (
+    <ul>
+      <Link to={"/home"}>Home</Link>
+    </ul>
+  );
+
+  const buttons = user ? (
+    <button id="logout-button" onClick={logoutHandler}>
+      Logout
+    </button>
+  ) : (
+    <button id="login/register-button">
+      <Link to={"/login"}>Login / Register</Link>
+    </button>
+  );
+
   return (
     <div className="navbar-container">
-      {user ? (
-        <button id="logout-button" onClick={logoutHandler}>
-          Logout
-        </button>
-      ) : (
-        <button id="login/register-button">
-          <Link to={"/login"}>Login / Register</Link>
-        </button>
-      )}
+      {links}
+      {buttons}
     </div>
   );
 }
