243998a19a67445a12c1faf4c59cb9ccb18612b2
Matthew McLendon <matthewmclendon1006@gmail.com>
2023-10-13 15:15:41 -0500

    func: added react-router-dom, set up loginPage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import "./App.css";
-import UserForm from "./components/users/UserForm";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navigation/Navbar";
 import UserProvider from "./components/users/UserProvider";
+import LoginPage from "./components/routes/LoginPage";
 
 function App() {
   return (
-    <UserProvider>
-      <Navbar />
-      <UserForm />
-    </UserProvider>
+    <>
+      <UserProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </UserProvider>
+    </>
   );
 }
 
