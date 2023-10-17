df6807329c0a48a990bc8e772dc492477040a041
Matthew McLendon <matthewmclendon1006@gmail.com>
2023-10-17 14:15:10 -0500

    func: settings and user update form visibility toggle

--- src/components/users/UserSettings.js
+++ src/components/users/UserSettings.js
@@ -5,33 +5,65 @@ import Card from "../style/card";
 
 export default function UserSettings() {
   const { user, deleteUser } = useContext(UserContext);
-  const [visible, setVisible] = useState(false);
+
+  const [isVisible, setIsVisible] = useState(false);
+  const [isUpdating, setIsUpdating] = useState(false);
 
   const showSettings = () => {
-    setVisible(true);
+    setIsVisible(true);
   };
 
   const hideSettings = () => {
-    setVisible(false);
+    setIsVisible(false);
   };
 
   const deleteUserHandler = () => {
     deleteUser(user);
   };
 
+  const showUpdateFormHandler = () => {
+    setIsVisible(false);
+    setIsUpdating(true);
+  };
+
+  const updateUserHandler = (event) => {
+    event.preventDefault();
+
+    setIsUpdating(false);
+  };
+
   const settings = (
     <Card>
       <div className="settings-container">
-        <button id="close-button" onClick={hideSettings}>X</button>
+        <button id="close-button" onClick={hideSettings}>
+          X
+        </button>
         <button onClick={deleteUserHandler}>Delete account</button>
+        <button onClick={showUpdateFormHandler}>
+          Update account information
+        </button>
       </div>
+    </Card>
+  );
+
+  const updateForm = (
+    <Card>
+      <form className="userUpdate-form">
+        <input type="text" placeholder="New username" />
+        <input type="password" placeholder="New password" />
+        <input type="email" placeholder="New email" />
+        <button onClick={updateUserHandler}>Submit</button>
+      </form>
     </Card>
   );
 
   return (
     <>
       {user ? <button onClick={showSettings}>Settings</button> : null}
-      {visible ? settings : null}
+      {isVisible ? settings : null}
+      {isUpdating ? updateForm : null}
     </>
   );
 }
+
+// continue here: work on updating user profile.
