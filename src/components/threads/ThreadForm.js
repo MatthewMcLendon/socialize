aa1a2007bde0a4d7289795e55b828aeb490bee66
Matthew McLendon <matthewmclendon1006@gmail.com>
2023-10-19 20:04:09 -0500

    func: search users and show selected users for moderator on ThreadForm

--- src/components/threads/ThreadForm.js
+++ src/components/threads/ThreadForm.js
@@ -1,27 +1,63 @@
 import "./ThreadForm.css";
 import Card from "../style/Card";
+import { UserContext } from "../users/UserProvider";
+import { useContext, useState } from "react";
 
 export default function ThreadForm() {
+  const { users, user } = useContext(UserContext);
+
+  const [moderators, setModerators] = useState([]);
+
   const submitHandler = (event) => {
     event.preventDefault();
 
     console.log("Submit");
   };
 
-  return (
-    <form onSubmit={submitHandler}>
-      <input type="hidden" />
-      <input type="text" placeholder="Thread name" required />
-      <textarea
-        name="description"
-        id=""
-        cols="30"
-        rows="10"
-        placeholder="Thread description"
-      />
-      <input type="text" placeholder="Moderators" />
+  const addModeratorHandler = () => {
+    const formText = document.querySelector("#moderator-search").value;
+    const moderator = moderatorCheck(formText);
 
-      <button type="submit">Submit</button>
-    </form>
+    if (moderator) {
+      setModerators([...moderators, moderator]);
+      document.querySelector("#moderator-search").value = "";
+    }
+  };
+
+  const moderatorCheck = (username) => {
+    const foundModerator = users.find((user) => user.username === username);
+    return foundModerator;
+  };
+
+  const moderatorList = (
+    <ul className="moderator-list">
+      {moderators.map((moderator) => {
+        return <li key={moderator.id}>{moderator.username}</li>;
+      })}
+    </ul>
+  );
+
+  return (
+    <Card>
+      <form id="thread-form" onSubmit={submitHandler}>
+        <input type="hidden" />
+        <input type="text" placeholder="Thread name" required />
+        <textarea
+          name="description"
+          id=""
+          cols="30"
+          rows="10"
+          placeholder="Thread description"
+        />
+        <input type="search" id="moderator-search" placeholder="Moderators" />
+        <button type="button" onClick={addModeratorHandler}>
+          Add moderator
+        </button>
+        {moderatorList}
+      </form>
+      <button type="submit" form="thread-form">
+        Submit
+      </button>
+    </Card>
   );
 }
