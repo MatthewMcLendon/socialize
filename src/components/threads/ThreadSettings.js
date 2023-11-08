f8e804d2060b0515f5aa5b63b852ba494d00dc89
Matthew McLendon <matthewmclendon1006@gmail.com>
2023-11-08 14:37:18 -0600

    func: toggle settings card visibility. Basic form and data

--- src/components/threads/ThreadSettings.js
+++ src/components/threads/ThreadSettings.js
@@ -1,6 +1,7 @@
 import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ThreadContext } from "./ThreadProvider";
+import Card from "../style/Card";
 
 export default function ThreadSettings({ thread }) {
   const { deleteThread } = useContext(ThreadContext);
@@ -10,18 +11,52 @@ export default function ThreadSettings({ thread }) {
 
   const deleteHandler = () => {
     deleteThread(thread);
-    navigate("/home");
+    navigate("/");
   };
 
   const visibilityHandler = () => {
     setIsVisible(true);
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const data = getFormData();
+    console.log(data);
+  };
+
+  const getFormData = () => {
+    const data = {
+      name: document.querySelector("#thread-update-form").value,
+      description: document.querySelector("#thread-description").value,
+    };
+
+    return data;
+  };
+
   const threadSettings = (
-    <>
+    <Card>
       <h1>Menu</h1>
+      <button
+        onClick={() => {
+          setIsVisible(false);
+        }}
+      >
+        X
+      </button>
       <button onClick={deleteHandler}>Delete thread</button>
-    </>
+      <form id="thread-update-form" onSubmit={submitHandler}>
+        <input type="text" placeholder={thread.name} />
+        <textarea
+          name="description"
+          id="thread-description"
+          cols="30"
+          rows="10"
+          placeholder={thread.description}
+        ></textarea>
+        <button>Update</button>
+      </form>
+    </Card>
   );
 
   return (
