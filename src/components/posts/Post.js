cab770ead3ee1b868a75c6efd6ab123601deddb1
Matthew McLendon <matthewmclendon1006@gmail.com>
2023-11-21 13:37:49 -0600

    func: added post details.

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -1,9 +1,24 @@
+import { useContext, useState, useEffect } from "react";
+import { UserContext } from "../users/UserProvider";
+import { Link } from "react-router-dom";
 import Card from "../style/card";
 
 export default function Post({ post }) {
+  const { users } = useContext(UserContext);
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    if (users) {
+      setUser(users.find((user) => user.id === post.user));
+    }
+  }, [users, post]);
+
   return (
     <Card>
-      <p>{post.title}</p>
+      <h3>{post.title}</h3>
+      <Link to={`/profile/${user.id}`}>{user.username}</Link>
+      {post.image ? <img src={post.image} alt="" /> : null}
+      <p>{post.text}</p>
     </Card>
   );
 }
