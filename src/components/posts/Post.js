f5533002f536ef716f57b5c3147ead58e69c8538
Matthew McLendon <matthewmclendon1006@gmail.com>
2023-12-07 14:04:33 -0600

    func: post like button, display number of likes

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -1,21 +1,28 @@
 import { useContext, useState, useEffect } from "react";
 import { UserContext } from "../users/UserProvider";
 import { ThreadContext } from "../threads/ThreadProvider";
+import { PostContext } from "../posts/PostProvider";
 import { Link, useNavigate } from "react-router-dom";
 import Card from "../style/card";
 
 export default function Post({ post, setIsEditing, isEditable, isModerator }) {
-  const { users } = useContext(UserContext);
+  const { users, user } = useContext(UserContext);
   const { threads } = useContext(ThreadContext);
+  const { updatePost } = useContext(PostContext);
 
-  const [user, setUser] = useState([]);
+  const [poster, setPoster] = useState([]);
   const [thread, setThread] = useState({});
 
   const navigate = useNavigate();
 
+  const likeHandler = () => {
+    post.likes.push(user.id);
+    updatePost(post);
+  };
+
   useEffect(() => {
     if (users && threads) {
-      setUser(users.find((user) => user.id === post.user));
+      setPoster(users.find((user) => user.id === post.user));
       setThread(threads.find((thread) => thread.id === post.thread));
     }
   }, [users, post, threads]);
@@ -26,7 +33,7 @@ export default function Post({ post, setIsEditing, isEditable, isModerator }) {
 
   return (
     <Card>
-      {user && thread ? (
+      {poster && thread ? (
         <>
           <div className="post-container" onClick={navigationHandler}>
             <h3>{post.title}</h3>
@@ -44,10 +51,13 @@ export default function Post({ post, setIsEditing, isEditable, isModerator }) {
           </div>
           <div className="post-links">
             <p>
-              Posted by <Link to={`/profile/${user.id}`}>{user.username}</Link>{" "}
-              in <Link to={`/threads/${thread.id}`}>{thread.name}</Link>
+              Posted by{" "}
+              <Link to={`/profile/${poster.id}`}>{poster.username}</Link> in{" "}
+              <Link to={`/threads/${thread.id}`}>{thread.name}</Link>
             </p>
           </div>
+          <button onClick={likeHandler}>Like!</button>
+          <p>{post.likes.length}</p>
         </>
       ) : (
         <p>Loading...</p>
