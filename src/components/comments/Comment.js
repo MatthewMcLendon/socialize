6ecb36e4e89de0fdd9b493104a61ecb57e8e55cc
Matthew McLendon <matthewmclendon1006@gmail.com>
2024-02-01 13:42:37 -0600

    chore: added comments and rewrote comment component

--- src/components/comments/Comment.js
+++ src/components/comments/Comment.js
@@ -3,22 +3,23 @@ import { UserContext } from "../users/UserProvider";
 import { Link } from "react-router-dom";
 import Card from "../style/card";
 
+// Format and render comment data from comment prop
 export default function Comment({ comment }) {
-  const { users } = useContext(UserContext);
-
+  // import getUserById function from UserContext, save output to user state
+  const { getUserById } = useContext(UserContext);
   const [user, setUser] = useState({});
 
+  // sync with db
   useEffect(() => {
-    const getUserData = () => {
-      const user = users.find((user) => user.id === comment.user);
+    const getUser = async () => {
+      var user = await getUserById(comment.user);
       setUser(user);
     };
 
-    if (users) {
-      getUserData();
-    }
-  }, [users, comment]);
+    getUser();
+  }, [comment]);
 
+  // render
   return (
     <Card>
       {user ? <Link to={`/profile/${user.id}`}>{user.username}</Link> : null}
--- src/components/comments/CommentForm.js
+++ src/components/comments/CommentForm.js
@@ -28,7 +28,7 @@ export default function CommentForm({ post }) {
           placeholder="Comment"
           required
         />
-        <input type="submit" />
+        <input type="submit" value={"Post"}/>
       </form>
     </Card>
   );
