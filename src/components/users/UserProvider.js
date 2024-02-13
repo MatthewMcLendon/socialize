9abf7339716f919409bdd7bbec0d654ef724a141
Matthew McLendon <matthewmclendon1006@gmail.com>
2024-02-13 15:25:17 -0600

    chore: added comments to UserProvider, added getUserByName route

--- src/components/users/UserProvider.js
+++ src/components/users/UserProvider.js
@@ -1,19 +1,25 @@
 import { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+// export context
 export const UserContext = createContext();
 
+// provider user and related data
 export default function UserProvider(props) {
+  // states for all users and logged in user
   const [user, setUser] = useState();
   const [users, setUsers] = useState();
 
+  // useNavigate shortcut for redirect after logout
   const navigate = useNavigate();
 
+  // on initialization, get all users and check locatstorage for logged in user
   useEffect(() => {
     getUsers();
     returningUserCheck();
   }, []);
 
+  // check localstorage for logged in user, if found set user state
   const returningUserCheck = () => {
     const returningUser = JSON.parse(localStorage.getItem("user"));
     if (returningUser) {
@@ -21,17 +27,20 @@ export default function UserProvider(props) {
     }
   };
 
+  // save user data to localstorage and set user state. used in children components
   const logInUser = (user) => {
     localStorage.setItem("user", JSON.stringify(user));
     setUser(user);
   };
 
+  // clear localstorage, set user state to default, navigate to home page. used in children components
   const logOutUser = () => {
     localStorage.clear();
     setUser();
     navigate("/");
   };
 
+  // post route. log in user after creation
   const addUser = (user) => {
     return fetch("http://localhost:8088/users", {
       method: "POST",
@@ -42,12 +51,14 @@ export default function UserProvider(props) {
     }).then(logInUser(user));
   };
 
+  // delete route
   const deleteUser = (user) => {
     return fetch(`http://localhost:8088/users/${user.id}`, {
       method: "DELETE",
     }).then(logOutUser);
   };
 
+  // put route
   const updateUser = (user) => {
     return fetch(`http://localhost:8088/users/${user.id}`, {
       method: "PUT",
@@ -60,18 +71,26 @@ export default function UserProvider(props) {
       .then(getUsers);
   };
 
+  // get route for all users
   const getUsers = () => {
     return fetch("http://localhost:8088/users")
       .then((response) => response.json())
       .then((response) => setUsers(response));
   };
 
+  // get route for specific user by id
   const getUserById = (id) => {
-    return fetch(`http://localhost:8088/users/${id}`).then((response) =>
+    return fetch(`http://localhost:8088/users/${id}`).then((response) => response.json());
+  };
+
+  // get route for specific user by username
+  const getUserByName = (name) => {
+    return fetch(`http://localhost:8088/users?username=${name}`).then((response) =>
       response.json()
     );
   };
 
+  // create provider and pass states / functions
   return (
     <UserContext.Provider
       value={{
@@ -83,6 +102,7 @@ export default function UserProvider(props) {
         deleteUser,
         updateUser,
         getUserById,
+        getUserByName,
       }}
     >
       {props.children}
